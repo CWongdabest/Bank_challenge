1d94ac1f9dadec8500609e6cb885d6350f971b11
Canace Wong <canacewongtoday@hotmail.com>
2017-11-27 12:29:28 +0000

    added add transaction functionality to bank account

--- lib/bankaccount.rb
+++ lib/bankaccount.rb
@@ -2,12 +2,23 @@ class Bankaccount
 
 DEFAULT_BALANCE = 0
 
-attr_reader :balance, :transactions 
+attr_reader :balance
 
   def initialize
     @balance = DEFAULT_BALANCE
     @transactions = []
   end
+
+  def transactions
+    @transactions
+  end
+
+  def add_transaction(date, transaction)
+    @balance += transaction
+    @transactions.push([date, transaction, @balance])
+  end
+
+
 
 
 
--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -2,6 +2,7 @@ require 'bankaccount'
 
 describe Bankaccount do
 
+
   it 'starts a bank account with a default bank balance' do
     bankaccount = Bankaccount.new
     expect(bankaccount.balance).to eq Bankaccount::DEFAULT_BALANCE
@@ -10,6 +11,12 @@ describe Bankaccount do
   it 'instantiates with an empty list of transactions' do
     bankaccount = Bankaccount.new
     expect(bankaccount.transactions.count).to eq 0
+  end
+
+  it 'records individual transaction' do
+      bankaccount = Bankaccount.new
+      bankaccount.add_transaction('1/10/2017', 30)
+      expect(bankaccount.transactions).to eq ([['1/10/2017', 30, 30]])
   end
 
 
